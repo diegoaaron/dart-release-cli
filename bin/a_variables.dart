0ef8463680dddeaa0b74a22734b360baa6dd76be
Diego Aarón <diegoaarondv@gmail.com>
2022-06-15 23:23:37 -0500

    update teoria 1

---
+++ bin/a_variables.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  final x = "hola";
+
+  final String y = "adios";
+
+  final z = [1, 2, 3];
+
+  z.add(8);
+
+  print(z);
+
+  const l = [19, 20, 21];
+
+  //l.add(22);
+
+  print(l);
+}
